f8cf5f06e2fb85f6336287d9b0d53c04eddc472a
Nguyen Duy <141839660+hduy1405@users.noreply.github.com>
2024-10-04 12:15:26 +0700

    Delete bai2 directory

--- bai2/scrips.js
+++
@@ -1,9 +0,0 @@
-// const togglePassword = document.querySelector('#togglePassword');
-// const passwordField = document.querySelector("#password");
-
-// togglePassword.addEventListener('click', function(){
-//     const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-//     passwordField.setAttribute('type', type);
-
-//     this.src = type === 'password' ? 
-// })
